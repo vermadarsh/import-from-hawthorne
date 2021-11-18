63c9412270b1924a0ad201678291f890b5efe6a1
Mubin Mansuri <43163379+MubinMansuri@users.noreply.github.com>
2021-11-18 17:37:02 +0530

    working on Progress bar UI Design...

--- admin/js/import-from-hawthorne-admin.js
+++ admin/js/import-from-hawthorne-admin.js
@@ -38,4 +38,30 @@ jQuery( document ).ready( function( $ ) {
 		$('.dropdown').toggleClass('show');
     });
 
+	// progress bar js 
+	var i = 0;
+	function move() {
+	if (i == 0) {
+		i = 1;
+		var elem = document.querySelector(".importer-progress");
+		var value = document.querySelector(" .value");
+		console.log(elem);
+		var width = 0;
+		var id = setInterval(frame, 0);
+		function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+			i = 0;
+		} else {
+			width++;
+			elem.setAttribute("value", width);
+			value.innerHTML = width + "%";
+			value.style.width = width + "%";
+		}
+		}
+	}
+	}
+	move();
+
+
 } );
---
+++ admin/templates/pages/import-products.php
@@ -0,0 +1,28 @@
+<section class="import-from-hawthorne-wrapper">
+
+    <div class="card importing-card">
+        <h2 class="heading">Importing</h2>
+        <p>Your products are now being imported...</p>
+
+        <div class="progress-bar-wrapper">
+            <progress class="importer-progress" max="100" value="0">
+            </progress>
+            <span class="value">0%</span>
+        </div>
+    </div>
+
+    <div class="card finish-card woocommerce-progress-form-wrapper">
+        <h2 class="heading">Importing products from a CSV file</h2>
+        <p>This tool allows you to import (or merge) product data to your store from a CSV or TXT file.</p>
+
+        <div class="importer-done">
+            <span class="dashicons dashicons-yes-alt icon"></span>
+            <p>
+            Import complete! Failed to import <strong>26</strong> products.
+            <a href="#" class="woocommerce-importer-done-view-errors">View import log</a>.
+            File uploaded: <strong>wc-product-export-18-11-2021-1637230637423-1.csv</strong>
+            </p>
+        </div>
+    </div>
+
+</section>

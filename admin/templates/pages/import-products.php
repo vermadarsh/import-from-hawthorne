50e8fce30097f59ae1e103441484f5bedbce488a
MubinMansuri <mubinmansuri41@gmail.com>
2021-11-22 15:51:30 +0530

    log HTML Added..

--- admin/templates/pages/import-products.php
+++ admin/templates/pages/import-products.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is used for templating the Hawthorne products import feature.
  *
@@ -7,63 +8,93 @@
  * @subpackage Import_From_Hawthorne/admin/pages
  */
 
-defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+defined('ABSPATH') || exit; // Exit if accessed directly.
 
 // delete_transient( 'hawthorne_product_items' ); die;
 
 // Fetch the products data from the transient.
-$products = get_transient( 'hawthorne_product_items' );
+$products = get_transient('hawthorne_product_items');
 
 // See if there are products in the transient.
-if ( false === $products ) {
+if (false === $products) {
 	$products = hawthorne_fetch_products(); // Shoot the API to get products.
 
 	/**
 	 * Store the response data in a cookie.
 	 * This cookie data will be used to import the products in the database.
 	 */
-	if ( false !== $products ) {
-		set_transient( 'hawthorne_product_items', wp_json_encode( $products ), ( 60*60*12 ) );
+	if (false !== $products) {
+		set_transient('hawthorne_product_items', wp_json_encode($products), (60 * 60 * 12));
 	}
 } else {
 	// If you're here, the data is already in transients.
-	$products = json_decode( $products, true );
+	$products = json_decode($products, true);
 }
 
 // Get the count of the products.
-$total_products = count( $products );
+$total_products = count($products);
 ?>
 <div class="wrap">
-	<h1><?php esc_html_e( 'Import Products', 'import-from-hawthorne' ); ?></h1>
+	<h1><?php esc_html_e('Import Products', 'import-from-hawthorne'); ?></h1>
 	<section class="import-from-hawthorne-wrapper">
 		<div class="card importing-card">
-			<h2 class="heading"><?php esc_html_e( 'Importing', 'import-from-hawthorne' ); ?></h2>
-			<p class="importing-notice"><?php echo sprintf( __( 'Your products are now being imported... %1$s0%3$s of %2$s%4$s%3$s imported', 'import-from-hawthorne' ), '<span class="imported-count">', '<span class="total-products-count">', '</span>', $total_products ); ?></p>
+			<h2 class="heading"><?php esc_html_e('Importing', 'import-from-hawthorne'); ?></h2>
+			<p class="importing-notice"><?php echo sprintf(__('Your products are now being imported... %1$s0%3$s of %2$s%4$s%3$s imported', 'import-from-hawthorne'), '<span class="imported-count">', '<span class="total-products-count">', '</span>', $total_products); ?></p>
 			<div class="progress-bar-wrapper">
 				<progress class="importer-progress" max="100" value="0"></progress>
 				<span class="value">0%</span>
 			</div>
 		</div>
 
-		<div class="card finish-card" style="display: none;">
-			<h2 class="heading"><?php esc_html_e( 'Import Complete!', 'import-from-hawthorne' ); ?></h2>
+		<div class="card finish-card">
+			<!-- style="display: none;"> -->
+			<h2 class="heading"><?php esc_html_e('Import Complete!', 'import-from-hawthorne'); ?></h2>
 			<div class="importer-done">
 				<span class="dashicons dashicons-yes-alt icon"></span>
 				<p>
 					<?php
 					/* translators: 1: %s: total products count, 2: %s: strong tag open, 3: %s: strong tag closed */
-					echo sprintf( __( '%1$s products imported. Newly added products: %2$s12%3$s Updated products: %2$s23%3$s', 'import-from-hawthorne' ), $total_products, '<strong>', '</strong>' );
+					echo sprintf(__('%1$s products imported. Newly added products: %2$s12%3$s Updated products: %2$s23%3$s', 'import-from-hawthorne'), $total_products, '<strong>', '</strong>');
 					?>
 				</p>
 			</div>
+			<div class="wc-actions text-right">
+				<a class="button button-primary" href="<?php echo esc_url(admin_url('edit.php?post_type=product')); ?>"><?php esc_html_e('View products', 'import-from-hawthorne'); ?></a>
+				<a class="button button-secondary openCollapse_log" href="javascript:void(0);"><?php esc_html_e('View import log', 'import-from-hawthorne'); ?></a>
+			</div>
 			<div class="collapse-wrapper">
 				<div class="collapse-body">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores vitae iure unde possimus fugiat impedit nulla, tempora sed pariatur asperiores omnis consectetur. Tempora mollitia numquam eveniet, saepe error optio.
+					<table class="widefat wc-importer-error-log-table">
+						<thead>
+							<tr>
+								<th>Product</th>
+								<th>Reason for failure</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th><code>V-Neck T-Shirt, ID 208, SKU woo-vneck-tee</code></th>
+								<td>Error getting remote image http://localhost:8888/woocom-learning/wp-content/uploads/2021/08/vneck-tee-2.jpg. Error: A valid URL was not provided.</td>
+							</tr>
+							<tr>
+								<th><code>Hoodie, ID 209, SKU woo-hoodie</code></th>
+								<td>Error getting remote image http://localhost:8888/woocom-learning/wp-content/uploads/2021/08/hoodie-2.jpg. Error: A valid URL was not provided.</td>
+							</tr>
+							<tr>
+								<th><code>Hoodie with Logo, ID 210, SKU woo-hoodie-with-logo</code></th>
+								<td>Error getting remote image http://localhost:8888/woocom-learning/wp-content/uploads/2021/08/hoodie-with-logo-2.jpg. Error: A valid URL was not provided.</td>
+							</tr>
+							<tr>
+								<th><code>T-Shirt, ID 211, SKU woo-tshirt</code></th>
+								<td>Error getting remote image http://localhost:8888/woocom-learning/wp-content/uploads/2021/08/tshirt-2.jpg. Error: A valid URL was not provided.</td>
+							</tr>
+							<tr>
+								<th><code>Beanie, ID 212, SKU woo-beanie</code></th>
+								<td>Error getting remote image http://localhost:8888/woocom-learning/wp-content/uploads/2021/08/beanie-2.jpg. Error: A valid URL was not provided.</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
-			</div>
-			<div class="wc-actions text-right">
-				<a class="button button-primary" href="<?php echo esc_url( admin_url( 'edit.php?post_type=product' ) ); ?>"><?php esc_html_e( 'View products', 'import-from-hawthorne' ); ?></a>
-				<a class="button button-secondary openCollapse_log" href="javascript:void(0);"><?php esc_html_e( 'View import log', 'import-from-hawthorne' ); ?></a>
 			</div>
 		</div>
 	</section>
